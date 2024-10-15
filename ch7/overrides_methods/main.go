c89d61b3a442603f4ea250a23896651fe95a5ed0
Cleberson <pauluci.cleberson@gmail.com>
2024-10-14 21:21:09 -0300

    Overriding methods with embedded struct types

---
+++ ch7/overrides_methods/main.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+type Tea struct{}
+
+func (t *Tea) addWater() {
+	println("Adding Water")
+}
+
+func (t *Tea) addTeaPowder() {
+	println("Adding Tea Powder")
+}
+
+func (t *Tea) addSugar() {
+	println("Adding Sugar")
+}
+
+func (t *Tea) addMilk() {
+	println("Adding Milk")
+}
+
+func (t *Tea) heatUp() {
+	println("Heating up")
+}
+
+type GreenTea struct {
+	Tea
+}
+
+func (t *GreenTea) addTeaPowder() {
+	println("Adding Green Tea Powder")
+}
+
+func (t *GreenTea) Prepare() {
+	t.addWater()
+	t.addTeaPowder()
+	t.addSugar()
+	t.heatUp()
+	fmt.Println("--------------------")
+	fmt.Println("--------------------")
+}
+
+func (t *Tea) Prepare() {
+	t.addWater()
+	t.addTeaPowder()
+	t.addSugar()
+	t.addMilk()
+	t.heatUp()
+	fmt.Println("--------------------")
+	fmt.Println("--------------------")
+}
+
+func main() {
+	greenTea := GreenTea{}
+	greenTea.Prepare()
+
+	tea := greenTea.Tea
+	tea.Prepare()
+}
